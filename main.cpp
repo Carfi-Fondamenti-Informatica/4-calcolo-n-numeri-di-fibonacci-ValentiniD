b82f64c07943ed40eb368e1933537e693faf2fa4
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-03-30 16:42:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ using namespace std;
 int main() {
     int n=0, primo_n=1, secondo_n=1, terzo_n;
 
-    do{
-        cout << "Quanto vuoi lunga la serie numerica di Fibonacci?" << endl;
-        cin >> n;
-        if(n<2){
-            cout << "errore" << endl;
-        }else{
-           cout << primo_n << "\n" << secondo_n << endl;
-        }
-    }while(n<2);
+    cout << "Quanto vuoi lunga la serie numerica di Fibonacci?" << endl;
+    cin >> n;
 
-    for(int variabile=0;variabile<n;variabile++){
-        terzo_n=primo_n+secondo_n;
-        secondo_n=primo_n;
-        primo_n=terzo_n;
-        cout << terzo_n << "\n";
+    if(n<2){
+        cout << "errore" << endl;
+    }else{
+        cout << primo_n << "\n" << secondo_n << endl;
+
+        for(int variabile=0;variabile<n;variabile++){
+            terzo_n=primo_n+secondo_n;
+            secondo_n=primo_n;
+            primo_n=terzo_n;
+            cout << terzo_n << "\n";
+        }
+
     }
 
     return 0;
