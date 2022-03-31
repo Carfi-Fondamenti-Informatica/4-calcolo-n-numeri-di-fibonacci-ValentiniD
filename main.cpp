299f2817c1d7820533620d56f84116c25f178657
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-03-31 08:21:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int n=0, primo_n=1, secondo_n=1, terzo_n;
+    int n=0, primo_n=0, secondo_n=1, terzo_n;
 
     cout << "Quanto vuoi lunga la serie numerica di Fibonacci?" << endl;
     cin >> n;
@@ -10,15 +10,12 @@ int main() {
     if(n<2){
         cout << "errore" << endl;
     }else{
-        cout << primo_n << "\n" << secondo_n << endl;
-
         for(int variabile=0;variabile<n;variabile++){
             terzo_n=primo_n+secondo_n;
             secondo_n=primo_n;
             primo_n=terzo_n;
             cout << terzo_n << "\n";
         }
-
     }
 
     return 0;
